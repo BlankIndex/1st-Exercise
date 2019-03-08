c0e7699fb68842130904145db54dd0f954657c2a
BlankIndex <47448053+BlankIndex@users.noreply.github.com>
2019-03-08 19:01:45 +0800

    Add files via upload

---
+++ ticketcounter.cpp
@@ -0,0 +1,193 @@
+#include <iostream>
+#include <math.h>
+
+
+int main(){
+using namespace std;
+
+//for game 1
+        int s1;
+		int p1;
+		int b1;
+		int g1;
+		
+		//operators for prices
+		
+		    cout << "input the ticket sales for general admission" << endl;
+		    cin >> g1;
+		
+		    cout << "input the ticket sales for box" << endl;
+		    cin >> b1;
+		    
+		      cout << "input the ticket sales for sideline " << endl;
+		    cin >> s1;
+		    
+		    cout<< "input the number of tickets sales for premium" <<endl;
+		    cin >> p1;
+		    
+	 //calculating the results 
+        int a;
+		a = g1*10;
+		int b;
+		b=s1*25;
+		int c;
+		c=b1*50;
+		int d;
+		d=p1*100;
+		
+    //print results
+
+		cout << "the sales for box for game 1 is "<< a << endl;
+		cout << "the sales for sideline for game 1 is " << b << endl;
+		cout << "the sales for premium for game 1 is "<< c << endl;
+		cout << "the sales for general admission for game 1 is " << d << endl;
+
+
+		
+//for game 2 
+
+
+        int s2;
+		int p2;
+		int b2;
+		int g2;
+		
+		
+		//operators for prices
+		
+		    cout << "input the ticket sales for general admission for game 2" << endl;
+		    cin >> g2;
+		
+		    cout << "input the ticket sales for box for game 2" << endl;
+		    cin >> b2;
+		    
+		      cout << "input the ticket sales for sideline for game 2 " << endl;
+		    cin >> s2;
+		    
+		    cout<< "input the number of tickets sales for premium for game 2" <<endl;
+		    cin >> p2;
+		    
+		     cout<< "" <<endl;
+		     cout<< "" <<endl;
+		     cout<< "" <<endl;
+		     
+	 //calculating the results 
+        int e;
+		e = g2*10;
+		int f;
+		f=s2*25;
+		int g;
+		g=b2*50;
+		int h;
+		h=p2*100;
+		
+    //print results
+
+		cout << "the sales for box for game 2 is "<< e << endl;
+		cout << "the sales for sideline for game 2 is " << f << endl;
+		cout << "the sales for premium for game 2 is "<< g << endl;
+		cout << "the sales for general admission for game 2 is " << h << endl;
+	
+	//for game 3 
+
+
+        int s3;
+		int p3;
+		int b3;
+		int g3;
+		
+		
+		//operators for prices
+		
+		    cout << "input the ticket sales for general admission for game 3" << endl;
+		    cin >> g3;
+		
+		    cout << "input the ticket sales for box for game 3" << endl;
+		    cin >> b3;
+		    
+		      cout << "input the ticket sales for sideline for game 3" << endl;
+		    cin >> s3;
+		    
+		    cout<< "input the number of tickets sales for premium for game 3" <<endl;
+		    cin >> p3;
+		    
+		     cout<< "" <<endl;
+		     cout<< "" <<endl;
+		     cout<< "" <<endl;
+		     
+	 //calculating the results 
+        int i;
+		i = g3*10;
+		int j;
+		j=s3*25;
+		int k;
+		k=b3*50;
+		int l;
+		l=p3*100;
+		
+    //print results
+
+		cout << "the sales for box for game 3 is "<< i << endl;
+		cout << "the sales for sideline for game 3 is " <<j << endl;
+		cout << "the sales for premium for game 3 is "<< k << endl;
+		cout << "the sales for general admission for game 3 is " << l << endl;	
+		
+	//for game 4 
+
+
+        int s4;
+		int p4;
+		int b4;
+		int g4;
+		
+		
+		//operators for prices
+		
+		    cout << "input the ticket sales for general admission for game 4" << endl;
+		    cin >> g4;
+		
+		    cout << "input the ticket sales for box for game 4" << endl;
+		    cin >> b4;
+		    
+		      cout << "input the ticket sales for sideline for game 4 " << endl;
+		    cin >> s4;
+		    
+		    cout<< "input the number of tickets sales for premium for game 4" <<endl;
+		    cin >> p4;
+		    
+		     cout<< "" <<endl;
+		     cout<< "" <<endl;
+		     cout<< "" <<endl;
+		     
+	 //calculating the results 
+        int m;
+		m = g3*10;
+		int n;
+		n=s3*25;
+		int o;
+		o=b3*50;
+		int p;
+		p=p3*100;
+		
+    //print results
+
+		cout << "the sales for box for game 4 is "<< m << endl;
+		cout << "the sales for sideline for game 4 is " << n << endl;
+		cout << "the sales for premium for game 4 is "<< o << endl;
+		cout << "the sales for general admission for game 4 is " << p << endl;	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+
+
+
+	return 0;
+
+};
